304462fd492b52b03a4ea62634fff21a4283ba12
Martijn Laarman <Mpdreamz@gmail.com>
2014-03-04 22:09:34 +0100

    improved test and made it print copy pastable numbers data (copy pastable into osx's Numbers for plotting)

--- src/Nest.Tests.Integration/Reproduce/ReproduceConnectionStallsTests.cs
+++ src/Nest.Tests.Integration/Reproduce/ReproduceConnectionStallsTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Sockets;
+using System.Runtime.Remoting.Channels;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Nest.Tests.MockData;
@@ -22,16 +23,19 @@ namespace Nest.Tests.Integration.Reproduce
 		{
 			this._index = ElasticsearchConfiguration.NewUniqueIndexName();
 			var people = NestTestData.Session.List<Person>(10000).Get().ToList();
-			var lotsOfPeople = Partition(people, 1000);
+			var lotsOfPeople = Partition(people, 1000).ToList();
 			var createIndexResult =  this._client.CreateIndex(_index, c => c
 				.NumberOfReplicas(0)
 				.NumberOfShards(1)
+				.Settings(s=>s.Add("index.refresh_interval", -1))
 				.AddMapping<Person>(m => m.MapFromAttributes())
 			);
 			Parallel.ForEach(lotsOfPeople, (listOfPeople) =>
 			{
 				_client.IndexMany(listOfPeople, _index);
 			});
+			_client.UpdateSettings(_index, new IndexSettings() {{"index.refresh_interval", "1s"}});
+			_client.Refresh(_index);
 
 		}
 
@@ -49,33 +53,47 @@ namespace Nest.Tests.Integration.Reproduce
 				timings.Add(timing);
 			}
 
+			timings.Should().OnlyContain(t => t.IsValid);
+
 			var minElasticsearch = timings.Min(t => t.ElasticsearchTook);
 			var minNest = timings.Min(t => t.NestTook);
 
 			//elasticsearch is fast! min should absolutely be below 2ms
-			minElasticsearch.Should().BeLessThan(2);
+			minElasticsearch.Should().BeLessThan(10);
 			
 			//nest has some overhead, it should be wthin reason though
-			minNest.Should().BeLessThan(5);
+			minNest.Should().BeLessThan(20);
 
 
 			var maxElasticsearch = timings.Max(t => t.ElasticsearchTook);
 			var maxNest = timings.Max(t => t.NestTook);
 
 			maxElasticsearch.Should().BeLessThan(20);
-			maxNest.Should().BeLessThan(20);
+			maxNest.Should().BeLessThan(30);
+
+			var plotElasticsearch = timings.Select(p => p.ElasticsearchTook).ToArray();
+			var plotNest = timings.Select(p => p.NestTook).ToArray();
+
+			plotElasticsearch.Should().NotBeEmpty();
+			plotNest.Should().NotBeEmpty();
+
+			var csv = string.Join("\n", timings.Select((t,i) => "{0}\t{1}\t{2}\t{3}".F(i, t.ElasticsearchTook, t.NestTook, t.IsValid)));
+			Assert.Pass(csv);
 
 		}
 
 		private Timings Search()
 		{
+			int take = 100;
 			var stopwatch = Stopwatch.StartNew();
-			var search = this._client.Search<Person>(s => s.MatchAll().Size(5000));
+			var search = this._client.Search<Person>(s => s.MatchAll().Size(take).Index(_index));
 			stopwatch.Stop();
 			return new Timings()
 			{
 				ElasticsearchTook = search.ElapsedMilliseconds,
-				NestTook = stopwatch.ElapsedMilliseconds
+				NestTook = stopwatch.ElapsedMilliseconds,
+				IsValid = search.IsValid && search.Documents.Count() == take
+				
 			};
 		}
 
@@ -83,6 +101,7 @@ namespace Nest.Tests.Integration.Reproduce
 		{
 			public long ElasticsearchTook { get; set; }
 			public long NestTook { get; set; }
+			public bool IsValid { get; set; }
 		}
 
 		
